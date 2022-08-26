08784934ff63d80bf5441d7662474c04ce6329d0
ArlequinTecno <arlequintecnologies@gmail.com>
2022-08-26 07:56:04 -0500

    SplashScreen funciona

--- app/src/main/java/com/arlequins/zoco/MainActivity.kt
+++ app/src/main/java/com/arlequins/zoco/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arlequins.zoco.databinding.ActivityMainBinding
 
 
@@ -18,10 +19,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        screenSplash.setKeepOnScreenCondition{
+            false
+        }
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
